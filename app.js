71336cdf440592446e0f43124b3f15d8aeb7c1b3
SeongBae-applepie <103756855+SeongBae-applepie@users.noreply.github.com>
2023-11-13 21:30:18 +0900

    4

--- app.js
+++ app.js
@@ -1,85 +1,72 @@
 const express = require("express");
 const app = express();
-const path = require("path")
+const path = require("path");
 const mysql = require("mysql2");
 const dbconfig = require("./public/config/db_config");
 const conn = mysql.createConnection(dbconfig);
-app.use(express.static(path.join(__dirname+"/public")));
+
+app.use(express.static(path.join(__dirname + "/public")));
 app.use(express.static(path.join("./public")));
-
-
 
 var port = 51713;
 
-app.get("/", function(req,res) {
-    res.sendFile(__dirname+"/public/home.html");
-})
-
-
-app.get('/db',function(req, res){
-const conn = mysql.createConnection(dbconfig);
-conn.connect(); // mysql과 연결
-var sql = 'select * from users2'
-conn.query(sql, function(err, rows, fields){
-        if (err) {
-            console.error('error connecting: ' + err.stack);
-        }
-        
-        res.send(rows);
-        
-});
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/public/home.html");
 });
 
+app.get("/db", function (req, res) {
+  //   const conn = mysql.createConnection(dbconfig);
+  //   conn.connect(); // mysql과 연결
+  //   var sql = "select * from users1";
+  //   conn.query(sql, function (err, rows, fields) {
+  //     if (err) {
+  //       console.error("error connecting: " + err.stack);
+  //     }
 
-
-app.get('/api', function(req,res) {
-    console.log(__dirname);
-    res.sendFile(__dirname+"/public/api.html");
+  //     res.send(rows);
+  //   });
+  res.sendFile(__dirname + "/public/db.html");
 });
 
-
-app.get('/m',function(req,res){
-   var result = get_db();
-   console.log(result);
-   res.send(result);
+app.get("/api", function (req, res) {
+  console.log(__dirname);
+  res.sendFile(__dirname + "/public/api.html");
 });
 
-app.get('/wbs',function(req,res){
-    res.sendFile(__dirname+"/public/wbs.html");
- });
+app.get("/m", function (req, res) {
+  var result = get_db();
+  console.log(result);
+  res.send(result);
+});
 
- app.get('/demo',function(req,res){
-    res.sendFile(__dirname+"/public/demo.html");
-   // res.end(__dirname+"/public/src/db.PNG")
-    
- });
+app.get("/wbs", function (req, res) {
+  res.sendFile(__dirname + "/public/wbs.html");
+});
+
+app.get("/demo", function (req, res) {
+  res.sendFile(__dirname + "/public/demo.html");
+  // res.end(__dirname+"/public/src/db.PNG")
+});
 
 app.listen(port, () => {
   //클라이언트 대기
   console.log("listening on ??? *:" + port);
 });
 
+function get_db() {
+  var result;
+  conn.connect(); // mysql과 연결
+  var sql = "select * from users2";
 
+  conn.query(sql, function (err, rows, fields) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+    }
 
-
-function get_db(){
-    var result;
-    conn.connect(); // mysql과 연결
-    var sql = 'select * from users2'
-
-    
-
-    conn.query(sql, function(err, rows, fields){
-        if (err) {
-            console.error('error connecting: ' + err.stack);
-        }
-
-      
-        console.log(rows[0].uuid);
-        result = rows[0][0];
-        console.log("res !!   "+result);
-        console.log("res !!   2"+rows);
-        return(result);
-    });
-    
+    console.log(rows[0].uuid);
+    result = rows[0][0];
+    console.log("res !!   " + result);
+    console.log("res !!   2" + rows);
+    return result;
+  });
 }
--- public/script/db_sc.js
+++ public/script/db_sc.js
@@ -1,15 +1,29 @@
-
-
-
-document.getElementById("text").i
-
-conn.connect(); // mysql과 연결
-var sql = 'select * from users2'
-conn.query(sql, function(err, rows, fields){
-        if (err) {
-            console.error('error connecting: ' + err.stack);
-        }
-    
-        text.innerText(rows); // rows: 쿼리 실행 결과물
+console.log("start db_sc");
+const mysql = require("mysql2");
+console.log(__dirname);
+const dbconfig = (module.exports = {
+  host: "localhost",
+  user: "root",
+  password: "1215",
+  database: "movie_Db",
 });
-conn.end();
+
+const conn = mysql.createConnection(dbconfig);
+
+window.onload = init();
+
+function init() {
+  var p = document.getElementById("text");
+
+  conn.connect(); // mysql과 연결
+  var sql = "select * from users1";
+  conn.query(sql, function (err, rows, fields) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+    }
+
+    res.send(rows);
+    p.innerText(rows[0]);
+  });
+  conn.end();
+}
